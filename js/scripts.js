93babe652d56596767c4309ac028eaf685cb2c46
bhimamalbhage18 <bhimamalbhage@hotmail.com>
2024-09-17 22:24:31 -0500

    Created Pokemon list in JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+    {name: 'Charizard', height: '0.6', types: ['fire', 'flying']},
+    {name: 'Spearow', height: '0.3', types: ['flying', 'normal']},
+];
