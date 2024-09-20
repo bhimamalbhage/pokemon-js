3ca1cbc5c3d077c3cd3402e628650124fea6b486
bhimamalbhage18 <bhimamalbhage@hotmail.com>
2024-09-20 00:39:48 -0500

    displaying pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ let pokemonList = [
     {name: 'Charizard', height: '0.6', types: ['fire', 'flying']},
     {name: 'Spearow', height: '0.3', types: ['flying', 'normal']},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    document.write('<p>' + pokemon.name + ' | height: ' + pokemon.height + '</p>');
+    if (pokemon.height >= 0.7) {
+        document.write('Wow, that’s big!');
+    }
+}
